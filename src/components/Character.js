ca595e93161756fd769265f5a2e63084ccd5c71d
petertrai <petertrai@gmail.com>
2023-05-24 16:54:26 -0700

    finish

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,28 @@
 // Write your Character component here
-import React, { useEffect, useState } from 'react';
+import React from "react"
+import App from '../App'
+
+const Character = (props) => {
+    console.log(props.data);
+
+    return (
+        <div className="character-wrapper">
+            <div>
+                {props.data.name}
+            </div>
+        </div>
+    )
+
+}
+
+export default Character
 
 
+// (6) [{…}, {…}, {…}, {…}, {…}, {…}]
+// 0: {name: 'Luke Skywalker', height: '172', mass: '77', hair_color: 'blond', skin_color: 'fair', …}
+// 1: {name: 'C-3PO', height: '167', mass: '75', hair_color: 'n/a', skin_color: 'gold', …}
+// 2: {name: 'R2-D2', height: '96', mass: '32', hair_color: 'n/a', skin_color: 'white, blue', …}
+// 3: {name: 'Darth Vader', height: '202', mass: '136', hair_color: 'none', skin_color: 'white', …}
+// 4: {name: 'Leia Organa', height: '150', mass: '49', hair_color: 'brown', skin_color: 'light', …}
+// 5: {name: 'Owen Lars', height: '178', mass: '120', hair_color: 'brown, grey', skin_color: 'light', …}length: 
 
-
-// const Character = () => {
-//     // const charState = [state, setState ] = useState(0);
-// const character = character
-
-//     return (
-//         <console.log({state})>
-
-//     )
-
-// }
-
-// export default Character
